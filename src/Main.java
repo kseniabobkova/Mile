473ef3015b9bc104c449afd3bd49111d89cd8832
kseniabobkova <bobkova@healbe.com>
2021-01-18 22:05:31 +0300

    inicial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price_InKopecks= 20_000_60;
+        int mile_price=20;
+        int bonus_mile = ticket_price_InKopecks / mile_price /100 ;
+
+        System.out.println(bonus_mile);
+    }
+}
+
