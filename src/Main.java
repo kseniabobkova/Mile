0025dfb9ffecbce6abd83d780a7625a6a7b3e242
kseniabobkova <bobkova@healbe.com>
2021-01-18 22:54:33 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price_InKopecks= 20_000_60;
-        int mile_price=20;
-        int bonus_mile = ticket_price_InKopecks / mile_price /100 ;
+        int ticket_price_InKopecks = 20_000_60;
+        int mile_price = 20;
+        int bonus_mile = ticket_price_InKopecks / mile_price / 100;
 
         System.out.println(bonus_mile);
     }
