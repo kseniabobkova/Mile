22f695b37e6d1f44adca17b1a6d2afec327256b4
kseniabobkova <bobkova@healbe.com>
2021-01-22 11:46:55 +0300

    BomusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPriceInKopecks = 20_000_60;
-        int milePrice = 20;
-        int bonusMile = ticketPriceInKopecks / milePrice / 100;
-
-        System.out.println(bonusMile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
 
